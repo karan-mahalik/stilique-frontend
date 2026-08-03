6310da900bc2c30a3da6060cd1ef93961f6d7b02
Karan Mahalik <karanmahalik88@gmail.com>
2026-08-03 16:00:05 +0530

    Add Vercel SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <ScrollTop/>
       <Routes>
-        <Route path="*" element={<App />} />
+        <Route path="/*" element={<App />} />
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutForm />} />
